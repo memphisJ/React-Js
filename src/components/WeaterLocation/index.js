733a5b055e4c6318a8d052f202022fdf562bd6c8
Hector Martinez <h.menphisxp@gmail.com>
2020-06-06 18:14:11 -0400

    Se cambio a WeatherLocation de un funtional component a un Class component

--- src/components/WeaterLocation/index.js
+++ src/components/WeaterLocation/index.js
@@ -1,4 +1,4 @@
-import React from 'react'; /* Importacion de React del paquete react */
+import React, { Component } from 'react'; /* Importacion de React del paquete react */
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css';
@@ -12,18 +12,20 @@ import {
 } from '../../constants/weathers';
 
 const data = {
-    temperature: 50,
+    temperature: 10,
     weatherState: SUN,
     humidity: 50,
     wind: '10 m/s',
 }
 
-const WeatherLocation = () => (   /* se define la constante WeatherLocation  y se usa un Arrow Function que defien un functional component*/
-    //<div>Weather Localtion from here</div>   /* codigo JSX  en el cuerpo del componente */
-    <div className="weatherLocationCont">
-        <Location city={"La Paz"}></Location>
-        <WeatherData data={data}></WeatherData>
-    </div>
-);
+class WeatherLocation extends Component {   /* se define la constante WeatherLocation  y se usa un Arrow Function que defien un functional component*/
+    render() {
+        return (
+        <div className="weatherLocationCont">
+            <Location city={"La Paz"}></Location>
+            <WeatherData data={data}></WeatherData>
+        </div>);
+    }
+};
 
 export default WeatherLocation; /* para que el resto de la aplicacion lo pueda tener disponible  */
